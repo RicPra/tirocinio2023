db816f32cd4235b4004d47fa53141b303b6b50dd
RicPra <114755903+RicPra@users.noreply.github.com>
2023-10-29 18:26:13 +0100

    Update and rename NDVIcoordinate.r to R_code_NDVIcoordinate.r

--- NDVIcoordinate.r
+++
@@ -1,6 +0,0 @@
-# Codice per trovarmi le gli NDVI corrispondenti alle coordinate date
-
-# Più o meno un grado di longitudine nella mia zona di interesse corrisponde a 80/90 km
-# Le coordinate le approssimerò quindi tutte a 3 cifre decimali, visto che la risolizione dei miei dati è di 300 metri.
-
-
---
+++ R_code_NDVIcoordinate.r
@@ -0,0 +1,27 @@
+# Codice per trovarmi gli NDVI corrispondenti alle coordinate date
+
+# Mi richiamo tutti i pacchetti che mi servono
+library(ncdf4)
+library(raster)
+library(ggplot2)
+library(RStoolbox)
+library(viridis)
+
+# Setto la work directory
+setwd("/Users/Ricky/Documents/NDVI")
+
+# Mi salvo la mia mappina
+ndvi <- raster("c_gls_NDVI300_201606110000_GLOBE_PROBAV_V1.0.1.nc")
+
+# Se voglio visualizzare la mappa me la plotto così
+ggplot() + geom_raster(ndvi, mapping = aes(x=x, y=y, fill=Normalized.Difference.Vegetation.Index.333M)) + scale_fill_viridis(option="mako")
+
+# Più o meno un grado di longitudine nella mia zona di interesse corrisponde a 80/90 km
+# Le coordinate le approssimerò quindi tutte a 3 cifre decimali, visto che la risolizione dei miei dati è di 300 metri.
+# Mi trovo il valore dell'NDVI rispetto alle coordinate che ho 
+# (nota: nella matrice va prima la longitudine e poi la latitudine)
+ndviValore <- extract(ndvi, matrix(c(11.061, 44.594), ncol=2))
+
+# Vediamo com'è
+ndviValore
+
