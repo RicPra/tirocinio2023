98c4ab1c7f2eac736a243f0e010a8d59c390b149
RicPra <114755903+RicPra@users.noreply.github.com>
2023-11-02 19:04:29 +0100

    Create R_code_NDVImean.r

---
+++ R_code_NDVImean.r
@@ -0,0 +1,30 @@
+# Codice per calcolare la media dei valori NDVI
+
+
+  if (...qualcosa...=="Oct_16") {
+
+    x <- 1
+repeat {
+  ndviValore2019 <- extract(ndvi2019, coordinate2019, ncol=2)
+    ndviValore2019 <- extract(ndvi2019, coordinate2019, ncol=2)
+    ndviValore2019 <- extract(ndvi2019, coordinate2019, ncol=2)
+    ndviValore2019 <- extract(ndvi2019, coordinate2019, ncol=2)
+    ndviValore2019 <- extract(ndvi2019, coordinate2019, ncol=2)
+    ndviValore2019 <- extract(ndvi2019, coordinate2019, ncol=2)
+    ndviValore2019 <- extract(ndvi2019, coordinate2019, ncol=2)
+    ndviValore2019 <- extract(ndvi2019, coordinate2019, ncol=2)
+    ndviValore2019 <- extract(ndvi2019, coordinate2019, ncol=2)
+    ndviValore2019 <- extract(ndvi2019, coordinate2019, ncol=2)
+    ndviValore2019 <- extract(ndvi2019, coordinate2019, ncol=2)
+    ndviValore2019 <- extract(ndvi2019, coordinate2019, ncol=2)
+
+   
+  x <- x + 1
+  if (x > 157) {
+    break
+  }
+}
+
+
+ 
+}
